a50832816ce6c137e3d24eecb67aae77f2e22fcf
mohamed anter <mohamed7jgd@gmail.com>
2026-06-27 01:38:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,49 @@ const ranks=[
 alert(ranks[Math.floor(Math.random()*ranks.length)]);
 
 }
+function randomMedal(){
+
+const medals=[
+
+"🥇 وسام تجاهل المكالمات",
+
+"🏆 نيشان الرد بعد يومين",
+
+"😂 وسام أكبر عاق",
+
+"📵 وسام الطيران وضع الصامت",
+
+"☕ وسام القهوة قبل أي طلب"
+
+];
+
+document.getElementById("medal").innerHTML=
+
+medals[Math.floor(Math.random()*medals.length)];
+
+}
+
+function randomQuote(){
+
+const quotes=[
+
+"العجلة من الشيطان... والرد بعدين أحسن.",
+
+"من نام بعد العصر فقد أدى واجبه الوطني.",
+
+"لا تؤجل عمل اليوم... تجاهله حتى يختفي.",
+
+"إذا رن الهاتف فلعله أخطأ الرقم.",
+
+"كل طلب قابل للتأجيل."
+
+];
+
+document.getElementById("quote").innerHTML=
+
+quotes[Math.floor(Math.random()*quotes.length)];
+
+}
+
+randomMedal();
+randomQuote();
